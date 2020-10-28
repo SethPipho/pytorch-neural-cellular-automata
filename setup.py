69efda3c363c0199974900d9a35089de12ddcaec
Pipho <seth.pipho@wbbinc.com>
2020-10-28 17:20:56 -0400

    add package boilerplate

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(name='pytorch-neural-cellular-automata',
+      version='0.1',
+      description='An pytorch implementation of Growing Neural Cellular Automata',
+      url='https://github.com/SethPipho/pytorch-neural-cellular-automata',
+      author='Seth Pipho',
+      author_email='seth.pipho@gmail.com',
+      license='MIT',
+      packages=['pytorch_neural_ca'],
+       install_requires=[
+          'torch',
+          'torchvision',
+          'pillow'
+      ],
+      entry_points = {
+        'console_scripts': ['pytorch_neural_ca=pytorch_neural_ca.__main__:main'],
+      },
+      zip_safe=True)
